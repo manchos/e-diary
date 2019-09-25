2acfa3cfcd50724413e61a6eab73741d936ca000
manchos <vladm.ru@gmail.com>
2019-09-26 00:08:14 +0300

    add function bad schoolkid

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 
 class Schoolkid(models.Model):
@@ -50,11 +51,11 @@ class Lesson(models.Model):
     teacher = models.ForeignKey(Teacher, null=True, verbose_name='учитель', on_delete=models.CASCADE)
 
     timeslot = models.IntegerField('слот', db_index=True, help_text='Номер слота в расписании уроков на этот день.')
-    room = models.CharField('класс', db_index=True, help_text='Класс где проходят занятия.', max_length=50)
+    room = models.CharField('аудитория', db_index=True, help_text='Аудитория где проходят занятия.', max_length=50)
     date = models.DateField('дата', db_index=True)
 
     def __str__(self):
-        return f"{self.subject.title} {self.year_of_study}{self.group_letter}"
+        return (f"{self.subject.title} {self.year_of_study}{self.group_letter} {self.date}")
 
 
 class Mark(models.Model):
@@ -92,3 +93,70 @@ class Commendation(models.Model):
 
     def __str__(self):
         return f"{self.schoolkid.full_name}"
+
+
+schoolkid = Schoolkid.objects.filter(full_name__contains='Фролов Иван')[0]
+
+def fix_marks(schoolkid):
+    bad_marks = Mark.objects.filter(
+        schoolkid=schoolkid, points__in=(2, 3)
+    ).update(points=5)
+    # for mark in bad_marks:
+    #     mark.points = 5
+
+
+def remove_chastisements(schoolkid):
+    chastisments = Chastisement.objects.filter(schoolkid=schoolkid)
+    chastisments.delete()
+
+def get_random_commendation():
+    commendation_list = ('Молодец!', 'Отлично!', 'Хорошо!',
+                         'Гораздо лучше, чем я ожидал!',
+                         'Ты меня приятно удивил!',
+                         'Великолепно!',
+                         'Прекрасно!', 'Ты меня очень обрадовал!',
+                         'Именно этого я давно ждал от тебя!',
+                         'Сказано здорово – просто и ясно!',
+                         'Ты, как всегда, точен!',
+                         'Очень хороший ответ!', 'Талантливо!',
+                         'Ты сегодня прыгнул выше головы!',
+                         'Я поражен!', 'Уже существенно лучше!',
+                         'Потрясающе!', 'Змечательно!',
+                         'Прекрасное начало!', 'Так держать!',
+                         'Ты на верном пути!', 'Здорово!',
+                         'Это как раз то, что нужно!', 'Я тобой горжусь!',
+                         'С каждым разом у тебя получается всё лучше!',
+                         'Мы с тобой не зря поработали!',
+                         'Я вижу, как ты стараешься!', 'Ты растешь над собой!',
+                         'Ты многое сделал, я это вижу!',
+                         'Теперь у тебя точно все получится!',
+                         )
+    return random.choice(commendation_list)
+
+
+def create_commendation(child_name, lesson_name):
+    kid = Schoolkid.objects.filter(full_name__contains=child_name).get()
+    last_kid_lesson = Lesson.objects.filter(
+        year_of_study=kid.year_of_study,
+        group_letter=kid.group_letter,
+        subject__title__contains=lesson_name,
+    ).order_by('-date').get()
+
+    random_commendation = get_random_commendation()
+    # print(random_commendation)
+    Commendation.objects.create(
+        text=random_commendation,
+        created=last_kid_lesson.date,
+        schoolkid=kid,
+        subject=last_kid_lesson.subject,
+        teacher=last_kid_lesson.teacher,
+    )
+    #
+    # print(last_kid_lesson)
+
+
+
+from datacenter.models import create_commendation
+create_commendation('Фролов Иван', 'Математика')
+# #
+
